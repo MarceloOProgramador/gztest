ddd91b335f4eaf96f5c472e161860695d5b03413
Marcelo Pereira <marcelooprogramador@gmail.com>
2021-10-16 01:34:20 +0000

    Create Developer Ropository

---
+++ app/Repositories/DeveloperRepository.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\UseCases\Dtos\DtoInterface;
+
+class DeveloperRepository
+{
+
+    public function save(DtoInterface $dto)
+    {
+        
+        return TRUE;
+    }
+
+}
